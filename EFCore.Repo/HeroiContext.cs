8260461003f266e3931345cc7672d37258c338f4
Bruna Soares de Oliveira <bruna.oliveira@programmers.com.br>
2021-11-26 17:57:09 -0300

    First Commit

---
+++ EFCore.Repo/HeroiContext.cs
@@ -0,0 +1,28 @@
+﻿using EFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace EFCore.Repo
+{
+    public class HeroiContext : DbContext
+    {
+       
+        public HeroiContext(DbContextOptions<HeroiContext> options) : base(options){ }
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
---
+++ EFCore.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿using EFCore.Domain;
+using EFCore.Repo;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EFCore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly HeroiContext _context;
+        public ValuesController(HeroiContext context)
+        {
+            _context = context;
+        }
+        //GET: api/<ValuesController>
+        [HttpGet("filtro/{nome}")]
+        public ActionResult<IEnumerable<string>> GetFiltro(string nome)
+        {
+           //var listtHeroi = _context.Herois
+           //                 .where (h => h.Nome.Contains(nome))
+           //                  .ToList();
+
+           var listHeroi = (from heroi in _context.Herois
+                            where heroi.Nome.Contains(nome)
+                            select heroi).ToList();
+            return Ok(listHeroi);
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{nameHero}")]
+        public ActionResult Get(string nameHero)
+        {
+            var heroi = new Heroi { Nome = nameHero };
+          
+                _context.Herois.Add(heroi);
+                _context.SaveChanges();
+           
+            return Ok();
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
